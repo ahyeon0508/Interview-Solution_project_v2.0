a9035f36e0cd054c060b20ef67d5a9f4451fda0e
yejin <yejin@naver.com>
2020-12-31 18:54:53 +0900

    setting

---
+++ src/main/java/com/springboot/interview_solution/InterviewSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.interview_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/interview_solution/InterviewSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.interview_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
