1416ca06561cb471010532d03974c17da15dc01a
yejin013 <yejin013@naver.com>
2021-01-14 20:44:42 +0900

    mysql

--- src/test/java/com/springboot/interview_solution/MySQLConnectionTest.java
+++ src/test/java/com/springboot/interview_solution/MySQLConnectionTest.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 public class MySQLConnectionTest {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/interview?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/interview?useSSL=false&serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
     private static final String USER = "root";
     private static final String PASSWORD = "2018";
 
