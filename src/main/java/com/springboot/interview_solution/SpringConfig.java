25231527b6256185e0b848637fc3d9fe01a1d2b8
hyewon11 <hwj1104@naver.com>
2021-01-17 19:15:47 +0900

    add Member Bean

---
+++ src/main/java/com/springboot/interview_solution/SpringConfig.java
@@ -0,0 +1,28 @@
+package com.springboot.interview_solution;
+
+import com.springboot.interview_solution.repository.MemberDao;
+import com.springboot.interview_solution.repository.MemberDaoImpl;
+import com.springboot.interview_solution.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberDao());
+    }
+
+    @Bean
+    public MemberDao memberDao(){
+        return new MemberDaoImpl(dataSource);
+    }
+}
