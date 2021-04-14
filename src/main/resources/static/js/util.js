156ad8b1ecfe17ed764e31676f03f781d98c8ec6
hyewon11 <hwj1104@naver.com>
2021-04-14 22:38:03 +0900

    inter_start

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -75,6 +75,22 @@ function interstar(n) {
     }
 }
 
+function interviewQuestionCheck(n) {
+    if (confirm("이 질문을 내 질문으로 등록하시겠습니까?")) {
+        window.location.href = "/student/interview/check/" + n;
+    } else {
+        return false;
+    }
+}
+
+function interviewQuestionUnCheck(n) {
+    if (confirm("이 질문을 내 질문에서 취소하시겠습니까?")) {
+        window.location.href = "/student/interview/uncheck/" + n;
+    } else {
+        return false;
+    }
+}
+
 function confirmUser(){
     alert("로그인이 필요합니다.");
     window.location.href = "/signin";
