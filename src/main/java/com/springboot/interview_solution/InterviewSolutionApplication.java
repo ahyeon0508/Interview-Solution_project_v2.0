a5245cb11f53d92d0df8d944ae0fbdf100c53a38
hyewon11 <hwj1104@naver.com>
2021-06-12 01:39:14 +0900

    load opencv library

--- src/main/java/com/springboot/interview_solution/InterviewSolutionApplication.java
+++ src/main/java/com/springboot/interview_solution/InterviewSolutionApplication.java
@@ -1,12 +1,17 @@
 package com.springboot.interview_solution;
 
+import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InterviewSolutionApplication {
-
+    static{
+        nu.pattern.OpenCV.loadShared();
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
     public static void main(String[] args) {
+        //OpenCV Library load
         SpringApplication.run(InterviewSolutionApplication.class, args);
     }
 
